a9ef595eb9fb7d301978ba50369b994d79e17cf2
gusbunce <gbunc2@eq.edu.au>
2021-06-05 17:37:11 +1000

    Pane.py Title Place Holder Created

--- oresat_tpane/pane.py
+++ oresat_tpane/pane.py
@@ -1,4 +1,27 @@
-from __future__ import annotations
-import curses
-import urwid as uw
-from abc import ABC, abstractmethod
+import urwid
+from urwid.display_common import _BOLD
+from urwid.widget import TOP
+
+# New Pane Class as the overall window for Tpane
+class Pane():
+
+    # Creates a function to create a new pane
+    def new_pane(self):
+
+        # Palette is a created list to be able to choose styling options for different parts of urwid
+        palette = [
+            ('header', 'white,bold', ''),
+        ]
+
+        # Creates a function to close the application
+        def exit_on_f10_input(key):
+            if key in ('f10', 'F10'):
+                urwid.ExitMainLoop()
+        
+        txt = urwid.Text(('header', u"Oresat Tpane"))
+        fill = urwid.Filler(txt, 'top')
+        loop = urwid.MainLoop(fill, palette, unhandled_input=exit_on_f10_input)
+        loop.run()
+
+pane = Pane()
+pane.new_pane()
