4ed762e2c8ac1cf91f2528cee7f5d710abeb06f1
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2022-01-03 10:40:58 -0500

    Fix dependency self-referencing error

--- oresat_tpane/__init__.py
+++ oresat_tpane/__init__.py
@@ -1,13 +1,10 @@
 """This module is responsible for providing a high-level interface for elements
 of Curses UI and general user interaction with the app,
 """
-from oresat_tpane.pane import Pane, VSplit, HSplit, TextFill
-from oresat_tpane.datagrid import DataGrid
-from urwid import MainLoop, ExitMainLoop
 
 MAJOR = 0
 MINOR = 0
-PATCH = 1
+PATCH = 2
 
 LIB_NAME = 'oresat-tpane'
 LIB_DESCRIPTION = 'This module is responsible for providing a high-level' \
@@ -24,13 +21,3 @@ LIB_ISSUES = 'https://github.com/oresat/oresat-tpane/labels/bug'
 
 MAINTAINER_NAME = 'Portland State Aerospace Society'
 MAINTAINER_EMAIL = 'oresat@pdx.edu'
-
-__all__ = [
-    "Pane",
-    "VSplit",
-    "HSplit",
-    "TextFill",
-    "MainLoop",
-    "DataGrid",
-    "ExitMainLoop",
-]
---
+++ tests/spec_hello_world.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+class HelloWorld(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_hello_world(self):
+        self.assertTrue(True)
